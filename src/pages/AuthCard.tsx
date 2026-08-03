10ea01c050069050b460a2209f23b44af76cd594
nadariqfranki-art <nadariqfranki@gmail.com>
2026-08-03 19:22:36 +0300

    Update AuthCard.tsx

--- src/pages/AuthCard.tsx
+++ src/pages/AuthCard.tsx
@@ -51,7 +51,8 @@ export default function AuthCard(props: AuthCardProps): JSX.Element {
   const useInputWrapper = () => props.inputWrapper !== false;
 
   return (
-    <div class={classNames(styles.card, props.class)}>
+    <div class={classNames(styles.card, props.class)} style="background-color: #AE8E5D;">
+
       {props.header}
       <Show when={useInputWrapper()} fallback={props.children}>
         <div class="input-wrapper">{props.children}</div>
